5ca54b4732ec141bcb5e120940056edc2c8d0def
nher69 <46374778+nher69@users.noreply.github.com>
2019-04-25 13:46:39 +0800

    lol
    l

--- app/components/Login.vue
+++ app/components/Login.vue
@@ -3,12 +3,11 @@
 		<ActionBar title="Axios" class="action-bar" />
         <StackLayout>
             <Button :text="textPicture" class="btn btn-primary" marginTop="20" @tap="takePicture"></Button>
-            <Image :src="pictureFromCamera"></Image>
+            <!-- <Image :src="pictureFromCamera"></Image> -->
             <Button :text="wtf" class="btn btn-primary" marginTop="20" @tap="apiCall"></Button>
         </StackLayout>
 	</Page>
 </template>
-
 
 <script>
 import * as http from "http";
@@ -26,9 +25,8 @@ export default {
             pictureAxios : null,
             wtf: "api call",
             facePPURL: 'https://api-us.faceplusplus.com/facepp/v3/detect?',
-            api_key: 'api_key=vJxq_XE6sSKG-0OtbZRIR5Mqr4Z8kKQs&',
-            api_secret: 'api_secret=kxjFcEfF-dDV6-MGaEE6nYI5tKtCBvva&image_base64='
-           
+            api_key: 'api_key=staqi4c1MY8Ilev0xI1jbWc1UsqWk-Uw&',
+            api_secret: 'api_secret=C3ArAl6Y0OUzgu1BAAUqsWcGqkuOdpC1&image_base64='        
         };
     },
     methods: {
@@ -40,15 +38,15 @@ export default {
                     this.pictureAxios = imageAsset;
                     let source = new imageSource.ImageSource();
                     source.fromAsset(imageAsset).then(source => {
-                        this.pictureBase64String = source.toBase64String("jpeg", 20);
-                        console.log(this.pictureBase64String);
+                    this.pictureBase64String = source.toBase64String("jpeg", 20);
+                    console.log(this.pictureBase64String);
                 //HTTP request HERE
                 console.log("http here");
                 console.log(source.toBase64String("jpeg", 20));
            
                 http.request({
                 //url: "https://api.faceid.com/faceid/v1/detect?" + "api_key=TEHWc-8gNFqinV-470D3Uh0Z-3teYxxL&api_secret=CrziqRzjXTVuhU4vjV7IxFNij2HNhE8i&image=" +source,
-                url: this.facePPURL + this.api_key + this.api_secret + source.toBase64String("jpeg", 20),                           
+                url: this.facePPURL + this.api_key + this.api_secret + this.pictureBase64String,                           
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 content: JSON.stringify({
@@ -59,7 +57,8 @@ export default {
                         console.log("results starts here");
                         console.log(result);
                         console.log("results ends here");
-                        Toast.makeText(result, "long").show();
+                   
+                   
                     }, error => {
                         console.error(error);
                     }); 
@@ -68,33 +67,28 @@ export default {
                 })    
         },
         apiCall(){
-            //HTTP request HERE
-                console.log("http here");
-                console.log(this.pictureBase64String);
+            Toast.makeText("Hello World", "long").show();
+            // //HTTP request HERE
+            //     console.log("http here");
+            //     console.log(this.pictureBase64String);
            
-                http.request({
-                url: "https://api.faceid.com/faceid/v1/detect?" + "api_key=TEHWc-8gNFqinV-470D3Uh0Z-3teYxxL&api_secret=CrziqRzjXTVuhU4vjV7IxFNij2HNhE8i&image=" +source,
-                //url: this.facePPURL + this.api_key + this.api_secret + this.image_base64 + this.pictureBase64String,                           
-                method: "POST",
-                headers: { "Content-Type": "application/json" },
-                content: JSON.stringify({})
-                    }).then(response => {
-                        var result = response.content.toJSON();
-                        console.log("results starts here");
-                        console.log(result);
-                        console.log("results ends here");
-                        Toast.makeText(result, "long").show();
-                    }, error => {
-                        console.error(error);
-                    }); 
-            //ENDS HERE         
+            //     http.request({
+            //     url: "https://api.faceid.com/faceid/v1/detect?" + "api_key=TEHWc-8gNFqinV-470D3Uh0Z-3teYxxL&api_secret=CrziqRzjXTVuhU4vjV7IxFNij2HNhE8i&Image=" +source,
+            //     //url: this.facePPURL + this.api_key + this.api_secret + this.image_base64 + this.pictureBase64String,                           
+            //     method: "POST",
+            //     headers: { "Content-Type": "application/json" },
+            //     content: JSON.stringify({})
+            //         }).then(response => {
+            //             var result = response.content.toJSON();
+            //             console.log("results starts here");
+            //             console.log(result);
+            //             console.log("results ends here");
+            //             Toast.makeText(result, "long").show();
+            //         }, error => {
+            //             console.error(error);
+            //         }); 
+            // //ENDS HERE         
             }
         }  
     }
-
-        
-
-    
-      
-
 </script>
