d537fe8cba4cd5b0f9139a644df6e29de6f742a1
nher69 <46374778+nher69@users.noreply.github.com>
2019-04-29 19:32:30 +0800

    x
    x

--- app/components/Login.vue
+++ app/components/Login.vue
@@ -1,7 +1,9 @@
 <template>
 	<Page class="page">
 		<ActionBar title="Axios" class="action-bar" />
+        
         <StackLayout>
+            <TextView text="pictureBase64" v-model="textFieldPicture" />
             <Button :text="textPicture" class="btn btn-primary" marginTop="20" @tap="takePicture"></Button>
             <!-- <Image :src="pictureFromCamera"></Image> -->
             <Button :text="wtf" class="btn btn-primary" marginTop="20" @tap="apiCall"></Button>
@@ -14,11 +16,16 @@ const bgHttp = require("nativescript-background-http");
 const fs = require("file-system");
 import * as FormData from 'form-data';
 import * as http from "http";
-import * as axios from "axios/dist/axios";
+import axios from "axios";
 import * as imageSource from "tns-core-modules/image-source";
 import * as camera from "nativescript-camera";
 import * as Toast from 'nativescript-toast';
+const enums = require("ui/enums");
 import { createReadStream } from 'fs';
+import { TNSHttpFormData, TNSHttpFormDataParam, TNSHttpFormDataResponse } from 'nativescript-http-formdata';
+import { formattedTextProperty } from 'tns-core-modules/ui/text-base/text-base';
+
+
 
 
 export default {
@@ -32,7 +39,9 @@ export default {
             file: null,
             facePPURL: 'https://api-us.faceplusplus.com/facepp/v3/detect?',
             api_key: 'api_key=staqi4c1MY8Ilev0xI1jbWc1UsqWk-Uw&',
-            api_secret: 'api_secret=C3ArAl6Y0OUzgu1BAAUqsWcGqkuOdpC1&image_base64='
+            api_secret: 'api_secret=C3ArAl6Y0OUzgu1BAAUqsWcGqkuOdpC1&image_base64=',
+            textFieldPicture: '',
+            image:''
         };
     },
     methods: {
@@ -41,25 +50,32 @@ export default {
             camera                
                 .takePicture({ width: 300, height: 300, keepAspectRatio: false, saveToGallery: false, cameraFacing: 'front' })
                 .then(imageAsset => {
-                    this.imageFile = imageAsset;
+                    this.image = imageAsset.android;
+                  
                     let source = new imageSource.ImageSource();
                     source.fromAsset(imageAsset).then(source => {
-                    this.pictureBase64String = source.toBase64String("jpeg", 20);
+                    this.pictureBase64String = source.toBase64String(enums.ImageFormat.jpg);
+        
                     console.log(this.pictureBase64String);
-                                
-                    //HTTP request HERE
+                    console.log(imageAsset);
+                    Toast.makeText(this.pictureBase64String, "long").show();
+                    let fd = new FormData();
+                    fd.append('image', this.image)            
+                    console.log(fd);
+
+                   //HTTP request HERE
+                    
                             http.request({
-                            url: "https://api.faceid.com/faceid/v1/detect?" + "api_key=TEHWc-8gNFqinV-470D3Uh0Z-3teYxxL&api_secret=CrziqRzjXTVuhU4vjV7IxFNij2HNhE8i",
-                            data:{image: imageAsset},
+                            url: "https://api.faceid.com/faceid/v1/detect?api_key=TEHWc-8gNFqinV-470D3Uh0Z-3teYxxL&api_secret=CrziqRzjXTVuhU4vjV7IxFNij2HNhE8i",
+                            data: fd,
                             method: "POST",
                             headers: { 
-                                "Content-Type": 'multipart/form-data'
+                                "Content-Type": 'multipart/form-data '
                             },
                             content: {
-                                "image": this.pictureFromCamera
+                               
                             }
                             }).then(response => {
-                                
                                 var result = response.content.toJSON();
                                 console.log("results starts here");
                                 console.log(result);
@@ -68,49 +84,20 @@ export default {
                                 console.error(error);
                             }); 
                         //ENDS HERE  
-
-                      //     axios({
-                //             method: "post",
-                //             url: "https://api.faceid.com/faceid/v1/detect?" + "api_key=TEHWc-8gNFqinV-470D3Uh0Z-3teYxxL&api_secret=CrziqRzjXTVuhU4vjV7IxFNij2HNhE8i",
-                //             data: 'image'+ data,
-                //             headers : {"Content-Type":" multipart/form-data"}      
-                //           })
-                //     .then(response => {
-                //                      var result = response.data;
-                //                      console.log(result);
-                //                  }, error => {
-                //                     console.error(error);
-                //                  });
-                // }) 
-
-
-
-
                     });
                })    
         },
         apiCall(){
             Toast.makeText("Hello World", "long").show();
-            // //HTTP request HERE
-            //     console.log("http here");
-            //     console.log(this.pictureBase64String);
-           
-            //     http.request({
-            //     url: "https://api.faceid.com/faceid/v1/detect?" + "api_key=TEHWc-8gNFqinV-470D3Uh0Z-3teYxxL&api_secret=CrziqRzjXTVuhU4vjV7IxFNij2HNhE8i&Image=" +source,
-            //     //url: this.facePPURL + this.api_key + this.api_secret + this.image_base64 + this.pictureBase64String,                           
-            //     method: "POST",
-            //     headers: { "Content-Type": "application/json" },
-            //     content: JSON.stringify({})
-            //         }).then(response => {
-            //             var result = response.content.toJSON();
-            //             console.log("results starts here");
-            //             console.log(result);
-            //             console.log("results ends here");
-            //             Toast.makeText(result, "long").show();
-            //         }, error => {
-            //             console.error(error);
-            //         }); 
-            // //ENDS HERE         
+            const fd = new FormData();
+            fd.append('image', this.imageFile, this.imageFile.name)
+            axios.post('https://api.faceid.com/faceid/v1/detect?api_key=TEHWc-8gNFqinV-470D3Uh0Z-3teYxxL&api_secret=CrziqRzjXTVuhU4vjV7IxFNij2HNhE8i',fd)
+            .then(response => {
+                var result = response.content.toJSON();
+                console.log(result);
+                }, error => {
+                console.error(error);
+                });
             }
         }  
     }
